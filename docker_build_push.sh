e0e5006df509b877f82f4f7e3c2296fa4478cfe9
Jean Francois Perrin <4800084+jfperrin@users.noreply.github.com>
2018-05-09 15:14:51 +0200

    Update heroku script

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -3,10 +3,10 @@
 docker login -u $HEROKU_USER -p "$HEROKU_TOKEN"  registry.heroku.com
 
 if [ "$1" = "tag" ]; then
-  HEROKU = $HEROKU_APP_TAGGED
+  heroku_app = $HEROKU_APP_TAGGED
 else
-  HEROKU = $HEROKU_APP
+  heroku_app = $HEROKU_APP
 fi
 
-docker build -t registry.heroku.com/$HEROKU/web .
-docker push registry.heroku.com/$HEROKU/web:latest
+docker build -t "registry.heroku.com/$heroku_app/web" .
+docker push "registry.heroku.com/$heroku_app/web:latest"
