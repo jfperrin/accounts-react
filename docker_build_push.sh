06a486f33a13d1aa8bd8fedbfe5da54d914f7491
Jeff <4800084-jfperrin@users.noreply.github.com>
2018-08-09 09:32:56 +0200

    Change travis.yml

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 docker login -u $HEROKU_USER -p "$HEROKU_TOKEN"  registry.heroku.com
-
-if [ "$1" = "tag" ]; then
-  heroku_app=$HEROKU_APP_TAGGED
-else
-  heroku_app=$HEROKU_APP
-fi
-
-docker build -t "registry.heroku.com/$heroku_app/web" .
-docker push "registry.heroku.com/$heroku_app/web:latest"
+docker build -t "registry.heroku.com/reactapp-testing/web" .
+docker push "registry.heroku.com/reactapp-testing/web:latest"
