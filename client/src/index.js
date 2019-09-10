05a31f02a5cbd7b3dbf55ca69064e52fa8c5d11f
Jean-François PERRIN <jf@perrin.at>
2019-09-10 09:15:40 +0200

    Update index.js
    Remove injectTapEventPlugin

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,10 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import registerServiceWorker from './registerServiceWorker';
 import createStore from './app/store/store';
 import App from './app/components/App';
 import './index.css';
-
-injectTapEventPlugin();
 
 const history = createHistory();
 const store = createStore(history);
