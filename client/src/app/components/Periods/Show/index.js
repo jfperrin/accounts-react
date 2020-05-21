f0018b4e45b4e68d8b3c12a18b112d55a8a508e1
jfperrin <4800084-jfperrin@users.noreply.github.com>
2020-05-21 12:58:25 +0200

    Hooks with graphql

--- client/src/app/components/Periods/Show/index.js
+++ client/src/app/components/Periods/Show/index.js
@@ -1,101 +1,108 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { graphql } from 'react-apollo';
-import * as compose from 'lodash.flowright';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import ImportIcon from '@material-ui/icons/ImportExport';
 import Operations from '../../Operations';
 import Balances from '../../Balances';
-import query from '../gqlQueries/get';
-import addRecurrentOperations from '../gqlQueries/addRecurrentOperations';
-import { updateLayoutTitle as updateLayoutTitleAction } from '../../../actions/ui/layout/title';
+import addRecurrentOperationsMutation from '../gqlQueries/addRecurrentOperations';
+import get from '../gqlQueries/get';
+import { updateLayoutTitle } from '../../../actions/ui/layout/title';
 import Index from '../../common/Button';
 
-class Period extends Component {
-  addRecurrentOperations(id) {
-    const { addRecurrentOperations, data } = this.props;
+const line = {
+  display: 'flex',
+  marginBottom: '15px',
+};
 
-    addRecurrentOperations({
-      variables: {
-        id,
-      },
-    }).then(() => data.refetch());
+const block = {
+  flex: 1,
+  padding: '5px',
+  marginRight: '5px',
+  border: 'solid 1px #F1F1F1',
+};
+
+const Period = ({ match }) => {
+  const dispatch = useDispatch();
+  const [addRecurrentOperations] = useMutation(addRecurrentOperationsMutation);
+  const { loading, data, refetch } = useQuery(get, { variables: { id: match.params.id } });
+
+  useEffect(() => {
+    if (data && !loading) {
+      dispatch(updateLayoutTitle(`Operations#${data.period.display}`));
+    }
+  }, [loading]);
+
+  if (!data) return null;
+
+  const handleAddRecurrentOperations = async () => {
+    await addRecurrentOperations({ variables: { id: match.params.id } });
+    await refetch();
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-  componentDidUpdate() {
-    const { data, updateLayoutTitle } = this.props;
-
-    if (!data.loading) {
-      updateLayoutTitle(`Operations#${data.period.display}`);
-    }
-  }
-
-  render() {
-    const { data, match } = this.props;
-
-    if (data.loading) {
-      return <div>Loading...</div>;
-    }
-
-    const line = {
-      display: 'flex',
-      marginBottom: '15px',
-    };
-
-    const block = {
-      flex: 1,
-      padding: '5px',
-      marginRight: '5px',
-      border: 'solid 1px #F1F1F1',
-    };
-
-    return (
-      <div className="operations">
-        <div style={line}>
-          <div style={block}>
-            <Balances idPeriod={match.params.id} />
-          </div>
-          <div style={block}>
-            <div style={{ display: 'flex' }}>
-              <div style={{ flex: 1 }}>
-                <h3 style={{ marginTop: '13px' }}>Solde</h3>
-              </div>
-              <div style={{ width: 45, paddingTop: '5px' }}>
-                <Index size={'small'} onClick={() => this.addRecurrentOperations(data.period.id)}>
-                  <ImportIcon />
-                </Index>
-              </div>
+  return (
+    <div className="operations">
+      <div style={line}>
+        <div style={block}>
+          <Balances idPeriod={match.params.id} />
+        </div>
+        <div style={block}>
+          <div style={{ display: 'flex' }}>
+            <div style={{ flex: 1 }}>
+              <h3 style={{ marginTop: '13px' }}>Solde</h3>
             </div>
-            <div style={{ display: 'flex', padding: '15px' }}>
-              <div style={{ width: '33%', whiteSpace: 'nowrap' }}>Opérations: {data.period.balance.operations.toFixed(2)} €</div>
-              <div style={{ width: '33%', textAlign: 'center', whiteSpace: 'nowrap' }}>Banque: {data.period.balance.banks.toFixed(2)} €</div>
-              <div style={{ width: '34%', textAlign: 'right', whiteSpace: 'nowrap' }}>Solde: {(data.period.balance.operations + data.period.balance.banks).toFixed(2)} €</div>
+            <div
+              style={{
+                width: 45,
+                paddingTop: '5px',
+              }}
+            >
+              <Index size={'small'} onClick={handleAddRecurrentOperations}>
+                <ImportIcon />
+              </Index>
+            </div>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              padding: '15px',
+            }}
+          >
+            <div
+              style={{
+                width: '33%',
+                whiteSpace: 'nowrap',
+              }}
+            >
+              Opérations: {data.period.balance.operations.toFixed(2)} €
+            </div>
+            <div
+              style={{
+                width: '33%',
+                textAlign: 'center',
+                whiteSpace: 'nowrap',
+              }}
+            >
+              Banque: {data.period.balance.banks.toFixed(2)} €
+            </div>
+            <div
+              style={{
+                width: '34%',
+                textAlign: 'right',
+                whiteSpace: 'nowrap',
+              }}
+            >
+              Solde: {(data.period.balance.operations + data.period.balance.banks).toFixed(2)} €
             </div>
           </div>
         </div>
-        <Operations idPeriod={match.params.id} />
       </div>
-    );
-  }
-}
+      <Operations idPeriod={match.params.id} />
+    </div>
+  );
+};
 
-function mapDispatchToProps(dispatch) {
-  return {
-    updateLayoutTitle: title => {
-      dispatch(updateLayoutTitleAction(title));
-    },
-  };
-}
-
-const mutations = compose(
-  graphql(addRecurrentOperations, {
-    name: 'addRecurrentOperations',
-  }),
-);
-
-export default mutations(
-  graphql(query, {
-    options: props => {
-      return { variables: { id: props.match.params.id } };
-    },
-  })(connect(null, mapDispatchToProps)(Period)),
-);
+export default Period;
