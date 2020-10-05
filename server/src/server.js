5ce59af70578206e8adc1912160c62c07b70beec
Jeff <4800084-jfperrin@users.noreply.github.com>
2020-10-05 09:39:27 +0200

    dotenv for dev process.env

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 /* eslint-disable prefer-template,no-path-concat,no-path-concat */
+import dotenv from 'dotenv';
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import mongoose from 'mongoose';
@@ -7,10 +8,13 @@ import session from 'express-session';
 import passport from 'passport';
 import schema from './schema/schema';
 
-const app = express();
 const MongoStore = require('connect-mongo')(session);
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://atraxi-dev:og9hAiz07eUNUQbI@accounts-dev.zlpfi.mongodb.net/accounts-dev?retryWrites=true&w=majority';
+// eslint-disable-next-line no-unused-vars
+const result = dotenv.config();
+const app = express();
+
+const { MONGO_URI } = process.env;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
@@ -48,7 +52,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('port', process.env.PORT || 4123);
+app.set('port', process.env.PORT);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
