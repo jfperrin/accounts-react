8617630132efaa98c69f85a01f87ff161acd67b9
Jeff <4800084-jfperrin@users.noreply.github.com>
2022-03-21 07:41:27 +0100

    Bug on ProtectedRoute

--- client/src/components/common/ProtectedRoute/index.jsx
+++ client/src/components/common/ProtectedRoute/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import currentUserQuery from '../../Users/gqlQueries/currentUser';
 
 const ProtectedRoute = ({ children }) => {
+  const location = useLocation();
   const { loading, data } = useQuery(currentUserQuery);
 
   if (!loading && !data.user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/login" state={{ from: location }} replace={true} />;
   }
 
   return children;
